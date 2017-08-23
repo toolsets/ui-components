1de878e8bdcc85b7e1c27b9b36c6a2916728a502
raf <raf@hummingbird.travel>
2017-08-23 12:26:12 +0500

    update
    Signed-off-by: raf <raf@hummingbird.travel>

---
+++ src/docs/examples/HelloWorld/ExampleHelloWorldTwo.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import HelloWorld from 'ui-components/HelloWorld'
+
+/** Everyone message  */
+export default function ExampleHelloWorld () {
+  return <HelloWorld message='Everyone' />
+}
